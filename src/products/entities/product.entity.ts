95cb280cbe784551c1afc1421e6a00fe79556329
Francisco <caviglia80@gmail.com>
2024-04-24 14:38:28 +0200

    first commit

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,74 @@
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text', {
+        unique: true,
+    })
+    title: string;
+
+    @Column('float', {
+        default: 0
+    })
+    price: number;
+
+    @Column({
+        type: 'text',
+        nullable: true
+    })
+    description: string;
+
+    @Column('text', {
+        unique: true
+    })
+    slug: string;
+
+    @Column('int', {
+        default: 0
+    })
+    stock: number;
+
+    @Column('text', {
+        array: true
+    })
+    sizes: string[];
+
+    @Column('text')
+    gender: string;
+
+    @Column('text', {
+        array: true,
+        default: []
+    })
+    tags: string[];
+
+    // images
+
+    @BeforeInsert()
+    checkSlugInsert() {
+        if (!this.slug) this.slug = this.title;
+
+        this.slug = this.slug
+            .toLowerCase()                  // Convertir a minúsculas
+            .trim()                         // Eliminar espacios al inicio y al final
+            .replaceAll(/\s+/g, '-')        // Reemplazar espacios con guiones
+            .replaceAll(/[^\w-]/g, '');     // Eliminar todos los caracteres no alfanuméricos excepto guiones
+    }
+
+    @BeforeUpdate()
+    checkSlugUpdate() {
+        this.slug = this.title;             // por si cambio el titulo vuelvo a generar el slug
+
+        this.slug = this.slug
+            .toLowerCase()                  // Convertir a minúsculas
+            .trim()                         // Eliminar espacios al inicio y al final
+            .replaceAll(/\s+/g, '-')        // Reemplazar espacios con guiones
+            .replaceAll(/[^\w-]/g, '');     // Eliminar todos los caracteres no alfanuméricos excepto guiones
+    }
+
+
+}
